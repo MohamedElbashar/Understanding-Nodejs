cbd6b4266ee7b97b49a1af704643e825799d1314
Mohamed Elbashar <mohamedbashar200@gmail.com>
2018-08-17 01:11:46 +0200

    Stream and performance

--- 07 HTTP and being a Web Server/outputtingHTML/app.js
+++ 07 HTTP and being a Web Server/outputtingHTML/app.js
@@ -2,12 +2,10 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+
     res.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    html = fs.readFileSync(__dirname + '/index.html', 'utf8');
-    var message = 'hello world...';
-    html = html.replace('{Message}', message);
-    res.end(html);
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
 
 }).listen(1337, '127.0.0.1');
