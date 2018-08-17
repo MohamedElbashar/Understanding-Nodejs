fb8628fba45b7e42146f65a678dca75b2e8d33ba
Mohamed Elbashar <mohamedbashar200@gmail.com>
2018-08-17 23:20:03 +0200

    Routing

--- 07 HTTP and being a Web Server/WebServer.js
+++ 07 HTTP and being a Web Server/WebServer.js
@@ -1,9 +1,22 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    res.end('Hello World\n');
-
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Hello World\n');
+    } else if (req.url === '/api') {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        var obj = {
+            first: 'john',
+            second: 'Doe'
+        };
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
 }).listen(1337, '127.0.0.1');
